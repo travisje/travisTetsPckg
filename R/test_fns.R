8c0a4c8e8e49e66ba63d03794635df6bf40619d1
Matthew Smith <msmith@alteryx.com>
2018-09-17 10:43:38 -0400

    Initial Commit

---
+++ R/test_fns.R
@@ -0,0 +1,36 @@
+#' Add One 
+#'
+#' This function adds one to a vector.
+#'
+#' @param x A numerical vector
+#' @return The vector plus one
+#' @export
+test_addone <- function(x) {
+  y <- x + 1
+  y
+}
+
+
+#' Times 2 
+#'
+#' This function multiplies a vector by 2
+#'
+#' @param x A numerical vector
+#' @return The vector times 2
+#' @export
+test_timestwo <- function(x) {
+  y <- x * 2
+  y
+}
+
+#' Yesihave 
+#'
+#' This function acknowlewdges itself
+#'
+#' @return A stringy greeting
+#' @export
+test_yesIhave <- function() {
+  y <- "Yep I got it"
+  y
+}
+
